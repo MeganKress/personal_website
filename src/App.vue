80f2a02c83ef6dd97259ab41d1618ff63af54aad
Megan Kress <MeganKress@users.noreply.github.com>
2022-10-08 13:28:32 -0500

    Update props

--- src/App.vue
+++ src/App.vue
@@ -53,20 +53,20 @@ let screenWidth = computed(() => {
      <span class="grid text-xl col-span-4 text-blue-200">Coding Resources</span>
   </div>
 
-  <input type="text" placeholder="Filter by search term..." v-model="searchTermString" class="m-5 rounded-lg p-2"  />
+  <input type="text" placeholder="Filter by search term..." v-model="props.searchTermString" class="m-5 rounded-lg p-2"  />
 
   <div class="w-full grid grid-cols-8 p-4 mt-2">
      <span class="grid text-xl col-span-4 text-blue-200">Organizations</span>
   </div>
   <div class="grid sm:grid-cols-4 grid-cols-2">
     <BlueCard name="Code.org" description="A nonprofit dedicated to providing access to computer science in K-12 schools. It is the organizer of the annual Hour of Code campaign."
-      url="https://code.org/" :searchTerm="searchTermString"/>
+      url="https://code.org/" :searchTerm="props.searchTermString"/>
     <BlueCard name="CSEdWeek" description='"Computer Science Education Week is an annual call to action to inspire K-12 students to learn computer science, advocate for equity, and celebrate the contributions of students, teachers, and partners to the field."'
-      url="https://www.csedweek.org/" :searchTerm="searchTermString"/>
+      url="https://www.csedweek.org/" :searchTerm="props.searchTermString"/>
     <BlueCard name="Computer Science Teachers Association" description="(CSTA) A teacher-led community that provides resources for K-12 educators."
-      url="https://www.csteachers.org/" :searchTerm="searchTermString"/>
+      url="https://www.csteachers.org/" :searchTerm="props.searchTermString"/>
     <BlueCard name="The National Center for Women & Information Technology" description="(NCWIT) An organization of change leaders aiming to increase participation of girls and women in computing in an intersectional way."
-      url="https://www.csteachers.org/" :searchTerm="searchTermString"/>
+      url="https://www.csteachers.org/" :searchTerm="props.searchTermString"/>
   </div>
 
   <div class="w-full grid grid-cols-8 p-4 mt-10">
@@ -74,29 +74,29 @@ let screenWidth = computed(() => {
   </div>
   <div class="grid sm:grid-cols-4 grid-cols-2">
     <BlueCard name="Coding Games for Beginners^^" description="A compilation of web-based games from HP Tech Takes"
-      url="https://www.hp.com/us-en/shop/tech-takes/coding-games-for-beginners" :searchTerm="searchTermString" />
+      url="https://www.hp.com/us-en/shop/tech-takes/coding-games-for-beginners" :searchTerm="props.searchTermString" />
     <BlueCard name="STEM Toys and Games^^" description="A collection of STEM activities from FUN.com"
-      url="https://www.fun.com/stem-toys-and-games.html" :searchTerm="searchTermString" />
+      url="https://www.fun.com/stem-toys-and-games.html" :searchTerm="props.searchTermString" />
     <BlueCard name="Arduino" description="Open source boards and software used for interactive projects. The website includes documentation, forums, and teacher guides and resources."
-      url="https://www.arduino.cc/" :searchTerm="searchTermString" />
+      url="https://www.arduino.cc/" :searchTerm="props.searchTermString" />
     <BlueCard name="Dash & Dot" description="Robots and apps to teach kids math and coding. Site includes learn to code curriculums and STEM funding guides."
-      url="https://www.makewonder.com/" :searchTerm="searchTermString" />
+      url="https://www.makewonder.com/" :searchTerm="props.searchTermString" />
     <BlueCard name="LEGO Mindstorms/LEGO Education SPIKE" description="Learning systems for the classroom to encourage creativity, collaboration, and critical thinking. Resources include lesson/unit plans and professional development information."
-      url="https://education.lego.com/en-us/" :searchTerm="searchTermString" />
+      url="https://education.lego.com/en-us/" :searchTerm="props.searchTermString" />
     <BlueCard name="Minecraft Education Edition" description="Lesson plans for the popular video game Minecraft that span STEM, Coding, SEL, History, Language Arts, and 21st Century Skills."
-      url="https://education.minecraft.net/en-us" :searchTerm="searchTermString" />
+      url="https://education.minecraft.net/en-us" :searchTerm="props.searchTermString" />
     <BlueCard name="Digital Dream Labs" description="Consumer robots and educational video games for learning how to code."
-      url="https://www.digitaldreamlabs.com/" :searchTerm="searchTermString" />
+      url="https://www.digitaldreamlabs.com/" :searchTerm="props.searchTermString" />
     <BlueCard name="Raspberry Pi" description="A UK-based nonprofit that produces low-cost single-board computers and software to advance the field of computing education."
-      url="https://www.raspberrypi.org/" :searchTerm="searchTermString" />
+      url="https://www.raspberrypi.org/" :searchTerm="props.searchTermString" />
     <BlueCard name="Robot Turtles" description="A board game for younger children (ages 3-8) that teaches the fundamentals of computer programming."
-      url="https://www.robotturtles.com/" :searchTerm="searchTermString" />
+      url="https://www.robotturtles.com/" :searchTerm="props.searchTermString" />
     <BlueCard name="Sphero" description="A collection of codeable robots, design-and-build kits, software, and curriculum guides for PK-12 education."
-      url="https://sphero.com/" :searchTerm="searchTermString" />
+      url="https://sphero.com/" :searchTerm="props.searchTermString" />
     <BlueCard name="Code Master^" description="A coding/logic game for kids 8 and up."
-      url="https://www.thinkfun.com/products/code-master/" :searchTerm="searchTermString" />
+      url="https://www.thinkfun.com/products/code-master/" :searchTerm="props.searchTermString" />
     <BlueCard name="lightbot" description="A game to solve puzzles through programming."
-      url="https://lightbot.com/" :searchTerm="searchTermString" />
+      url="https://lightbot.com/" :searchTerm="props.searchTermString" />
   </div>
 
 
@@ -105,19 +105,19 @@ let screenWidth = computed(() => {
   </div>
   <div class="grid sm:grid-cols-4 grid-cols-2">
     <BlueCard name="Codecademy" description="Free and paid coding lesson tracks."
-      url="https://www.codecademy.com/" :searchTerm="searchTermString" />
+      url="https://www.codecademy.com/" :searchTerm="props.searchTermString" />
     <BlueCard name="Code.org" description="Resources for parents and educators to spread CS education throughout the US."
-      url="https://www.code.org/" :searchTerm="searchTermString" />
+      url="https://www.code.org/" :searchTerm="props.searchTermString" />
     <BlueCard name="Khan Academy" description="Various HTML and JavaScript educational videos."
-      url="https://www.khanacademy.org/computing/computer-programming" :searchTerm="searchTermString" />
+      url="https://www.khanacademy.org/computing/computer-programming" :searchTerm="props.searchTermString" />
     <BlueCard name="freeCodeCamp" description="A free collection of lessons and certification tracks."
-      url="https://www.freecodecamp.org/" :searchTerm="searchTermString" />
+      url="https://www.freecodecamp.org/" :searchTerm="props.searchTermString" />
     <BlueCard name="MIT App Inventor" description="A visual programming environment to build apps for mobile devices."
-      url="https://appinventor.mit.edu/" :searchTerm="searchTermString" />
+      url="https://appinventor.mit.edu/" :searchTerm="props.searchTermString" />
     <BlueCard name="Scratch" description="A coding language to create and share stories, apps, and games."
-      url="https://scratch.mit.edu/" :searchTerm="searchTermString" />
+      url="https://scratch.mit.edu/" :searchTerm="props.searchTermString" />
     <BlueCard name="Tynker" description="A paid coding platform for kids for home and school use."
-      url="https://www.tynker.com/" :searchTerm="searchTermString" />
+      url="https://www.tynker.com/" :searchTerm="props.searchTermString" />
 
   </div>
 
